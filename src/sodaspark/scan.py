08b7607d8abe497632fd2a9da9228da8c061e9ab
Cor Zuurmond <jczuurmond@protonmail.com>
2021-08-31 17:25:25 +0200

    Re name scan_yml_path to scan_yml_file

--- src/sodaspark/scan.py
+++ src/sodaspark/scan.py
@@ -6,25 +6,25 @@ from pyspark.sql import DataFrame
 from sodasql.scan.scan import Scan
 
 
-def create(scan_yml_path: Union[str, Path]) -> Scan:
+def create(scan_yml_file: Union[str, Path]) -> Scan:
     """
     Create a scan object
 
     Parameters
     ----------
-    scan_yml_path : Union[str, Path]
+    scan_yml_file : Union[str, Path]
         The path to a scan file.
     """
     pass
 
 
-def execute(scan_yml_path: Union[str, Path], df: DataFrame) -> DataFrame:
+def execute(scan_yml_file: Union[str, Path], df: DataFrame) -> DataFrame:
     """
     Execute a scan on a data frame.
 
     Parameters
     ----------
-    scan_yml_path : Union[str, Path]
+    scan_yml_file : Union[str, Path]
         The path to a scan file.
     df: DataFrame
         The data frame to be scanned.
