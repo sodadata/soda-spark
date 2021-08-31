47914983054e8ffdfc4b8cb91a87eef16e8614d3
Cor Zuurmond <jczuurmond@protonmail.com>
2021-08-31 16:01:13 +0200

    Add scan module

---
+++ src/sodaspark/scan.py
@@ -0,0 +1,15 @@
+from typing import Union
+from pathlib import Path
+
+
+class Scan:
+    def __init__(self, scan_yml_path: Union[str, Path]) -> None:
+        """
+        Scan object.
+
+        Parameters
+        ----------
+        scan_yml_path : Union[str, Path]
+            The path to a scan file.
+        """
+        pass
