b5a56f7ea739aca7b659e55a43dfa68acd89219d
Cor Zuurmond <jczuurmond@protonmail.com>
2021-08-31 16:02:48 +0200

    Add execute method

--- src/sodaspark/scan.py
+++ src/sodaspark/scan.py
@@ -1,6 +1,8 @@
 from typing import Union
 from pathlib import Path
 
+from pyspark.sql import DataFrame
+
 
 class Scan:
     def __init__(self, scan_yml_path: Union[str, Path]) -> None:
@@ -11,5 +13,21 @@ class Scan:
         ----------
         scan_yml_path : Union[str, Path]
             The path to a scan file.
+        """
+        pass
+
+    def execute(self, df: DataFrame) -> DataFrame:
+        """
+        Execute a scan on a data frame.
+
+        Parameters
+        ----------
+        df: DataFrame
+            The data frame to be scanned.
+
+        Returns
+        -------
+        out : DataFrame
+            The scan results.
         """
         pass
