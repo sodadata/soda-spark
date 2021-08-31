e7bbf0df31149eed8987c7b329bf2e8bf6042c26
Cor Zuurmond <jczuurmond@protonmail.com>
2021-08-31 16:12:30 +0200

    Add fixture for scan

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -2,7 +2,14 @@ from pathlib import Path
 
 import pytest
 
+from sodaspark.scan import Scan
+
 
 @pytest.fixture
 def scan_data_frame_path() -> Path:
     return Path(__file__).parent.absolute() / "data/scan_data_frame.yml"
+
+
+@pytest.fixture
+def scan(scan_data_frame_path: Path) -> Scan:
+    return Scan(scan_data_frame_path)
