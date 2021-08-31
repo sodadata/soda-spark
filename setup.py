25847ad54599116daac8c71fd81a56b99aaf66f8
Cor Zuurmond <jczuurmond@protonmail.com>
2021-08-31 17:33:14 +0200

    Add soda sql spark as dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author="Soda",
     install_requires=[
         "soda-sql==2.0.0b27",  # TODO: Replace version pin with version range
+        "soda-sql-spark==2.1.0b14",  # TODO: Replace version pin with version range
         "pyspark>=3.0.0,<4.0.0",
     ],
     extras_require={
