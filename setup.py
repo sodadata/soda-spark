295cae84e190e47109e6c57d15cfdb9d2fdfcbdf
Cor <jczuurmond@protonmail.com>
2021-08-31 15:12:02 +0200

    Restructure repo and update tests (#17)
    * Move soda spark into src directory
    * Rename soda_spark to sodaspark
    * Bump dev versions
    * Adjust to setup to look for soda spark in src/
    * Add setup.cfg with pytest settings
    * Clean up tests from cookie cutter
    * Ignore pytest output
    * Delete example.py

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name="soda-spark",
-    packages=find_packages(),
+    packages=find_packages("src/"),
     version="0.1.0",
     description="Soda SQL API for PySpark data frame",
     author="Soda",
     extras_require={
         "dev": [
-            "pre-commit==2.2.0",
+            "pre-commit==2.14.1",
             "pytest-spark==0.6.0",
-            "pytest-cov==2.8.1",
-            "pyspark==2.4.5",
+            "pytest-cov==2.12.1",
+            "pyspark==3.1.2",
         ]
     },
+    package_dir={"": "src"},
 )
