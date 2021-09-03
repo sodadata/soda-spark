eb054a95f648a38cc7e66de2553dcdae44534dfe
Cor Zuurmond <jczuurmond@protonmail.com>
2021-09-03 14:41:27 +0200

    Clean up fixture

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -68,23 +68,15 @@ class Row:
 def df(spark_session: SparkSession) -> DataFrame:
     """A spark data frame to be used in the tests."""
 
+    date = dt.date(2021, 1, 1)
     id = "a76824f0-50c0-11eb-8be8-88e9fe6293fd"
     data = [
-        Row(id, "Paula Landry", 3006, dt.date(2021, 1, 1), "28,42 %", "UK"),
-        Row(
-            id,
-            "Kevin Crawford",
-            7243,
-            dt.date(2021, 1, 1),
-            "22,75 %",
-            "Netherlands",
-        ),
-        Row(id, "Kimberly Green", 6589, dt.date(2021, 1, 1), "11,92 %", "US"),
-        Row(id, "William Fox", 1972, dt.date(2021, 1, 1), "14,26 %", "UK"),
-        Row(
-            id, "Cynthia Gonzales", 3687, dt.date(2021, 1, 1), "18,32 %", "US"
-        ),
-        Row(id, "Kim Brown", 1277, dt.date(2021, 1, 1), "16,37 %", "US"),
+        Row(id, "Paula Landry", 3006, date, "28,42 %", "UK"),
+        Row(id, "Kevin Crawford", 7243, date, "22,75 %", "NL"),
+        Row(id, "Kimberly Green", 6589, date, "11,92 %", "US"),
+        Row(id, "William Fox", 1972, date, "14,26 %", "UK"),
+        Row(id, "Cynthia Gonzales", 3687, date, "18,32 %", "US"),
+        Row(id, "Kim Brown", 1277, date, "16,37 %", "US"),
     ]
 
     schema = T.StructType(
