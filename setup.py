8e0d84336931d9b743d2ba50abd34e34d744ff56
Cor Zuurmond <jczuurmond@protonmail.com>
2021-09-06 15:10:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-from setuptools import find_packages, setup
+import textwrap
+
+from setuptools import setup
 
 setup(
-    name="soda-spark",
-    packages=find_packages("src/"),
-    version="0.1.0.dev",
-    description="Soda SQL API for PySpark data frame",
-    author="Soda",
-    install_requires=[
-        "soda-sql-spark==2.1.0b14",  # TODO: Replace version pin with version range
-        "pyspark>=3.0.0,<4.0.0",
-    ],
-    extras_require={
-        "dev": [
-            "pre-commit==2.14.1",
-            "pytest-spark==0.6.0",
-            "pytest-cov==2.12.1",
-        ]
+    use_scm_version={
+        "write_to": "src/sodaspark/version.py",
+        "write_to_template": textwrap.dedent(
+            """
+             from __future__ import unicode_literals
+
+
+             __version__ = {version!r}
+             """,
+        ).lstrip(),
     },
-    package_dir={"": "src"},
 )
