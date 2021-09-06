0f177f1c2690a44f4ff37631529973e4e4e5fb2d
Cor <jczuurmond@protonmail.com>
2021-09-06 15:41:17 +0200

    Use setup config (#58)
    * Update setup.cfg
    
    * Update setup.py
    
    * Add version to init
    
    * Fix some setup.cfg things
    
    * Set scm in pyproject.tmol
    
    * Ignore file with version
    
    * Fix typo
    
    * Fix ignore version.py
    
    * Add pytest dependency
    
    * Make pytest-cov dependency open ended
    
    * Let tox install testing dependencies

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="soda-spark",
-    packages=find_packages("src/"),
-    version="0.1.0.dev",
-    description="Soda SQL API for PySpark data frame",
-    author="Soda",
-    install_requires=[
-        "soda-sql-spark==2.1.0b14",  # TODO: Replace version pin with version range
-        "pyspark>=3.0.0,<4.0.0",
-    ],
-    extras_require={
-        "dev": [
-            "pre-commit==2.14.1",
-            "pytest-spark==0.6.0",
-            "pytest-cov==2.12.1",
-        ]
-    },
-    package_dir={"": "src"},
-)
+setup(package_dir={"": "src"})
