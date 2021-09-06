d55feb5c5faa282a9b188408bf7c2136621d8166
Cor Zuurmond <jczuurmond@protonmail.com>
2021-09-06 15:24:56 +0200

    Set scm in pyproject.tmol

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-import textwrap
-
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        "write_to": "src/sodaspark/version.py",
-        "write_to_template": textwrap.dedent(
-            """
-             from __future__ import unicode_literals
-
-
-             __version__ = {version!r}
-             """,
-        ).lstrip(),
-    },
-    package_dir={"": "src"},
-)
+setup(package_dir={"": "src"})
